b44eb28dcbb2b761d972276fc7906bfa59478aac
TaiwoForbes <soyemihissa2000@gmail.com>
2023-11-01 15:18:00 +0100

    finished responsiveness for mobile view and added new files

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ export default {
         '4xl': ' 5.69261px 5.69261px 3.25292px 0px rgba(0, 0, 0, 0.25);',
       }
     },
+    screens: {
+      'ss': '300px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'lg': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 }
